84ef03d82676bdac25cd62242873fbd6369a3fdc
Gupta <sidhigupta@deloitte.com.au>
2023-05-17 12:05:36 +1000

    change code

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -42,7 +42,7 @@ export default function Profile({ employee }: ProfileProps) {
 }
 
 export async function getStaticPaths() {
-  const res = await fetch(`http://localhost:3000/api/get-ids`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/get-ids`);
   const employeeIds = await res.json();
   const paths = employeeIds.map((id: string) => ({
     params: { id },
@@ -53,9 +53,8 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }: StaticProps) {
   const res = await fetch(
-    `http://localhost:3000/api/get-employee-by-id?id=${params.id}`
+    `${process.env.NEXT_PUBLIC_API_ENDPOINT}/get-employee-by-id?id=${params.id}`
   );
-  console.log("HERE")
   const employee = await res.json();
   return { props: { employee } };
 }
