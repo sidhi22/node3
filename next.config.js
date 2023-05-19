e0678639024f05713ea8fcf81df7edfec0c75484
Gupta <sidhigupta@deloitte.com.au>
2023-05-19 13:52:16 +1000

    change

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
-    NEO4J_URI: process.env.NEO4J_URI,
-    NEO4J_USERNAME: process.env.NEO4J_USERNAME,
-    NEO4J_PASSWORD: process.env.NEO4J_PASSWORD
   }
 }
 
